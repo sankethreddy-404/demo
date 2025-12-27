3eff231268d52e3534106a5c94e4f89e9539d876
sankethreddy-404 <sankethreddychimpiri@gmail.com>
2025-12-27 18:59:21 +0530

    Day 2: REST API using @PathVariable

--- src/main/java/com/backend/demo/controller/HelloController.java
+++ src/main/java/com/backend/demo/controller/HelloController.java
@@ -1,11 +1,17 @@
 package com.backend.demo.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
 @RestController
 public class HelloController {
     @GetMapping("/hello")
     public String hello(){
         return "Backend is working";
     }
+    @GetMapping("/hellopath/{name}")
+    public String  hellopath(@PathVariable String name){
+        return "Hello " + name + " backend is working";
+    }
+
 
 }
