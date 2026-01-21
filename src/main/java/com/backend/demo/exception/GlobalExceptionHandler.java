d418fa5266fe16eab174a1d53fa2a8ef789a97dd
sankethreddy-404 <sankethreddychimpiri@gmail.com>
2026-01-21 18:22:34 +0530

    Day 23: Modified global exception handling with consistent API responses

--- src/main/java/com/backend/demo/exception/GlobalExceptionHandler.java
+++ src/main/java/com/backend/demo/exception/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.backend.demo.exception;
 
+import com.backend.demo.dto.APIResponse;
 import jakarta.validation.ConstraintViolation;
 import jakarta.validation.ConstraintViolationException;
 
@@ -9,16 +10,15 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import java.util.HashMap;
+import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<Map<String, List<String>>> handleMethodArgumentNotValid(
+    public ResponseEntity<APIResponse<List<String>>> handleMethodArgumentNotValid(
             MethodArgumentNotValidException ex) {
 
         List<String> errors = ex.getBindingResult()
@@ -27,15 +27,15 @@ public class GlobalExceptionHandler {
                 .map(error -> error.getDefaultMessage())
                 .toList();
 
-        Map<String, List<String>> response = new HashMap<>();
-        response.put("errors", errors);
+        APIResponse<List<String>> response = new APIResponse<>(LocalDateTime.now(),400,"validation failed",errors);
+
 
         return ResponseEntity.badRequest().body(response);
     }
 
 
     @ExceptionHandler(ConstraintViolationException.class)
-    public ResponseEntity<Map<String, List<String>>> handleConstraintViolation(
+    public ResponseEntity<APIResponse<List<String>>> handleConstraintViolation(
             ConstraintViolationException ex) {
 
         List<String> errors = ex.getConstraintViolations()
@@ -43,14 +43,19 @@ public class GlobalExceptionHandler {
                 .map(ConstraintViolation::getMessage)
                 .toList();
 
-        Map<String, List<String>> response = new HashMap<>();
-        response.put("errors", errors);
+        APIResponse<List<String>> response = new APIResponse(LocalDateTime.now(),400,"validation failed",errors);
 
         return ResponseEntity.badRequest().body(response);
     }
     @ExceptionHandler(UserNotFoundException.class)
-    public ResponseEntity<String> handleUserNotFound(UserNotFoundException ex){
-        return  ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    public ResponseEntity<APIResponse<Void>> handleUserNotFound(UserNotFoundException ex){
+        APIResponse<Void> response=new APIResponse(LocalDateTime.now(),404,ex.getMessage(),null);
+        return  ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
+    }
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<APIResponse<Void>> handleGenericException(Exception ex){
+        APIResponse<Void> response=new APIResponse(LocalDateTime.now(),500,"Internal server error",null);
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
     }
 
 
